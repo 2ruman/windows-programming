bb6306ed932687d968659b8aeba8f3939ef6be74
Truman <truman.t.kim@gmail.com>
2020-11-18 03:45:04 +0900

    Initial deployment for version 1.0.0

---
+++ winforms/ScreensaverPreventer/ScreensaverPreventer/MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace ScreensaverPreventer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.chbMouseEvent = new System.Windows.Forms.CheckBox();
+            this.chbKeyEvent = new System.Windows.Forms.CheckBox();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblWelcome.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(0, 0);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(287, 112);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "Hello";
+            this.lblWelcome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblWelcome.Click += new System.EventHandler(this.lblWelcome_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(12, 176);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(264, 53);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // chbMouseEvent
+            // 
+            this.chbMouseEvent.AutoSize = true;
+            this.chbMouseEvent.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chbMouseEvent.Location = new System.Drawing.Point(12, 139);
+            this.chbMouseEvent.Name = "chbMouseEvent";
+            this.chbMouseEvent.Size = new System.Drawing.Size(95, 18);
+            this.chbMouseEvent.TabIndex = 2;
+            this.chbMouseEvent.Text = "Mouse Event";
+            this.chbMouseEvent.UseVisualStyleBackColor = true;
+            this.chbMouseEvent.CheckedChanged += new System.EventHandler(this.chbMouseEvent_CheckedChanged);
+            // 
+            // chbKeyEvent
+            // 
+            this.chbKeyEvent.AutoSize = true;
+            this.chbKeyEvent.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chbKeyEvent.Location = new System.Drawing.Point(13, 115);
+            this.chbKeyEvent.Name = "chbKeyEvent";
+            this.chbKeyEvent.Size = new System.Drawing.Size(76, 18);
+            this.chbKeyEvent.TabIndex = 1;
+            this.chbKeyEvent.Text = "Key Event";
+            this.chbKeyEvent.UseVisualStyleBackColor = true;
+            this.chbKeyEvent.CheckedChanged += new System.EventHandler(this.chbKeyEvent_CheckedChanged);
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFinish.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFinish.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinish.Location = new System.Drawing.Point(13, 235);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(263, 53);
+            this.btnFinish.TabIndex = 3;
+            this.btnFinish.Text = "Finish";
+            this.btnFinish.UseVisualStyleBackColor = true;
+            this.btnFinish.Click += new System.EventHandler(this.btnFinish_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 299);
+            this.Controls.Add(this.chbKeyEvent);
+            this.Controls.Add(this.btnFinish);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.chbMouseEvent);
+            this.Controls.Add(this.lblWelcome);
+            this.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Screensaver Preventer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.CheckBox chbMouseEvent;
+        private System.Windows.Forms.CheckBox chbKeyEvent;
+        private System.Windows.Forms.Button btnFinish;
+    }
+}
+
---
+++ winforms/ScreensaverPreventer/ScreensaverPreventer/MainForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ScreensaverPreventer
+{
+    public partial class MainForm : Form
+    {
+        private static bool welcomeToggle = false;
+        private const int DELAY_MS = 3000;
+        private Thread mWorkThread = null;
+        private bool keyEventFlag = true;
+        private bool mouseEventFlag = true;
+
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pressLWinKey()
+        {
+            keybd_event((byte)Keys.LWin, 0x45, 0x00, 0);
+            keybd_event((byte)Keys.LWin, 0x45, 0x02, 0);
+        }
+
+        private void makeCursorRunawayFromCenter()
+        {
+            Cursor = new Cursor(Cursor.Current.Handle);
+            int centerX = Screen.PrimaryScreen.Bounds.Width / 2;
+            int centerY = Screen.PrimaryScreen.Bounds.Height / 2;
+            int destX, destY;
+
+            if( Cursor.Position.X > centerX )
+            {
+                destX = centerX - 200;
+            }
+            else
+            {
+                destX = centerX + 200;
+            }
+            destY = centerY;
+            Cursor.Position = new Point(destX, destY);
+        }
+
+        private void prevent()
+        {
+            while(true)
+            {
+                if( keyEventFlag )
+                {
+                    pressLWinKey();
+                }
+                if( mouseEventFlag)
+                {
+                    makeCursorRunawayFromCenter();
+                }
+                Thread.Sleep(DELAY_MS);
+            }
+        }
+
+        private void abort()
+        {
+            Debug.WriteLine("Abort!!!");
+            if (mWorkThread != null)
+            {
+                mWorkThread.Abort();
+                mWorkThread = null;
+            }
+
+            Environment.Exit(0);
+        }
+
+        private void lblWelcome_Click(object sender, EventArgs e)
+        {
+            if( welcomeToggle = !welcomeToggle )
+            {
+                lblWelcome.Text = "^.<";
+            }
+            else
+            {
+                lblWelcome.Text = "Hello";
+            }
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            chbKeyEvent.Enabled = chbMouseEvent.Enabled = btnStart.Enabled = false;
+            mWorkThread = new Thread(prevent);
+            mWorkThread.Start();
+        }
+
+        private void btnFinish_Click(object sender, EventArgs e)
+        {
+            abort();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            chbKeyEvent.Checked = keyEventFlag;
+            chbMouseEvent.Checked = mouseEventFlag;
+        }
+
+        private void chbKeyEvent_CheckedChanged(object sender, EventArgs e)
+        {
+            keyEventFlag = chbKeyEvent.Checked;
+        }
+
+        private void chbMouseEvent_CheckedChanged(object sender, EventArgs e)
+        {
+            mouseEventFlag = chbMouseEvent.Checked;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            abort();
+        }
+    }
+}
